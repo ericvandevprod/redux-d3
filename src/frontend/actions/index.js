0e7948936d7d8b6fa1b39b6de021935abde6a139
Eric Vandenberg <eric@vandevprod.com>
2017-03-12 00:26:13 -0800

    Weather reducer

--- src/frontend/actions/index.js
+++ src/frontend/actions/index.js
@@ -10,9 +10,9 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
 
-  console.log(url);
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
 
