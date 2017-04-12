17d550200def153a14ab2700983cdebaf3c2edc3
Eric Vandenberg <eric@vandevprod.com>
2017-04-11 22:05:32 -0700

    Set api key in define plugin for webpack

--- server/deploy.js
+++ server/deploy.js
@@ -2,20 +2,13 @@ if (process.env.NODE_ENV === 'production') {
   const child_process = require('child_process');
   const path = require('path');
 
-  const env = Object.assign({}, process.env);
-
-  const options = {
-    env: env
-  };
-
-
   const webpackExec = `${path.resolve(process.cwd(), 'node_modules/.bin/webpack')}`;
   const file = `${path.resolve(__dirname, 'webpack/webpack.production.config.js')}`;
   const command = `${webpackExec} -p --config ${file}`;
 
   console.log('dropped in with production env');
 
-  child_process.exec(command, options, (error, stdout, stderr) => {
+  child_process.exec(command, (error, stdout, stderr) => {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
@@ -23,3 +16,4 @@ if (process.env.NODE_ENV === 'production') {
     }
   });
 }
+
--- server/webpack/webpack.base.config.js
+++ server/webpack/webpack.base.config.js
@@ -31,7 +31,8 @@ module.exports = (options) => ({
   plugins: options.plugins.concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        WEATHER_API: JSON.stringify(process.env.WEATHER_API)
       }
     }),
     new webpack.NamedModulesPlugin()
