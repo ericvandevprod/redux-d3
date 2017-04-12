054a466616392f6066d1ed97a92da5c5788c410b
Eric Vandenberg <eric@vandevprod.com>
2017-04-12 12:41:07 -0700

    Proxy route set up for dark sky api to avoid cors

--- server/index.js
+++ server/index.js
@@ -1,26 +1,59 @@
+const dotenv = require('dotenv').config();
+
 const path = require('path');
+const axios = require('axios');
 const webpack = require('webpack');
 const express = require('express');
-const cors = require('cors');
+const bodyParser = require('body-parser');
+const historyApiFallback = require('connect-history-api-fallback');
 const WepackDevMiddleware = require('webpack-dev-middleware');
 const WebpackHotMiddleware = require('webpack-hot-middleware');
-
 const WebpackDevConfig = require('./webpack/webpack.development.config.js');
-
 
 const development = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/api', function(req, res) {
+    res.json({ message: 'Hi, welcome to the server api!' });
+});
+
+app.get('/api/darksky', function(req, res) {
+    const ROOT_WEATHER_URL = `https://api.darksky.net/forecast/${process.env.WEATHER_API}`;
+
+    try {
+        const coordinates = `${req.query.lat},${req.query.lng}`;
+        const url = `${ROOT_WEATHER_URL}/${coordinates}`;
+
+        axios.get(url)
+            .then(function(response) {
+                if (response.status !== 200) {
+                    res.status(response.status).json({'message': 'Bad response from Dark Sky server'});
+                }
+                return response.data;
+            })
+            .then(function(payload) {
+                res.status(200).json(payload);
+            })
+            .catch((e) => {
+                return e.message;
+            })
+    } catch(err) {
+        console.log("Errors occurs requesting Dark Sky API", err);
+        res.status(500).json({'message': 'Errors occurs requesting Dark Sky API', 'details' : err});
+    }
+});
 
 if (development) {
   const compiler = webpack(WebpackDevConfig);
+
   const middleware = WepackDevMiddleware(compiler, {
     publicPath: WebpackDevConfig.output.publicPath,
     contentBase: path.resolve(process.cwd(), "src/www"),
     hot: true,
-    historyApiFallback: true,
     stats: {
       colors: true,
       hash: false,
@@ -32,14 +65,20 @@ if (development) {
   });
 
   app.use(middleware);
+
+  app.use(historyApiFallback({
+      verbose: false
+  }));
+
+  app.use(middleware);
   app.use(WebpackHotMiddleware(compiler));
-  app.get('*', function response(req, res) {
+  app.get('/', function response(req, res) {
     res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
     res.end();
   });
 } else {
   app.use(express.static(path.resolve(process.cwd(), 'dist')));
-  app.get('*', function response(req, res) {
+  app.get('/', function response(req, res) {
     res.sendFile(path.join(process.cwd(), 'dist/index.html'));
   });
 }
--- src/app/actions/fetchWeather.js
+++ src/app/actions/fetchWeather.js
@@ -5,7 +5,6 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
   const encodedURI = encodeURIComponent(city);
   const geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedURI}`;
-  const ROOT_WEATHER_URL = `https://api.darksky.net/forecast/${process.env.WEATHER_API}`;
 
   const request = axios.get(geocodeURL)
       .then((response) => {
@@ -20,10 +19,10 @@ export function fetchWeather(city) {
 
         let lat = response.data.results[0].geometry.location.lat,
             lng = response.data.results[0].geometry.location.lng,
-            weatherUrl = `${ROOT_WEATHER_URL}/${lat},${lng}`;
+            proxyURL = `/api/darksky?lat=${lat}&lng=${lng}`;;
 
         console.log(response.data.results[0].formatted_address);
-        return axios.get(weatherUrl);
+        return axios.get(proxyURL);
       }).catch((error) => {
         return error;
   });
